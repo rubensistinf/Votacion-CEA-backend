f4226298fb86a51a8e6257fe78154703f3e6d94e
condorirubenlazo <condorirubenlazo@gmail.com>
2026-04-16 13:33:42 -0400

    Pro Level: Fix startup crash (missing Request import)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
